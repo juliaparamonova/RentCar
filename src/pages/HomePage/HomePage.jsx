a053126bca17a1f6696100f1333d97abdbd9c123
Julia <jpkoord@gmail.com>
2024-09-18 18:29:05 +0300

    favicon+hero

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,12 @@
+import clsx from 'clsx';
+import s from './HomePage.module.css';
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    // <div className={clsx(s.home)}>
+    <div className={clsx(s.image_container)}>test</div>
+    // </div>
+  );
 };
 
 export default HomePage;
